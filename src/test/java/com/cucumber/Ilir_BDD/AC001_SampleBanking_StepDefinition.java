f2ca524262bd426c34ea76c5238ff0f2f303133b
Shak19 <ilir.shakiri@gmail.com>
2020-05-14 19:04:37 -0400

    commit from eclipse

---
+++ src/test/java/com/cucumber/Ilir_BDD/AC001_SampleBanking_StepDefinition.java
@@ -0,0 +1,12 @@
+package com.cucumber.Ilir_BDD;
+
+import cucumber.api.DataTable;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.When;
+import cucumber.api.java.en.Then;
+
+public class AC001_SampleBanking_StepDefinition {
+
+	
+	
+}
